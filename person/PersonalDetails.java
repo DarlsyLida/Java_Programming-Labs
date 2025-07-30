5044d6c493398805cc5f5f2de0530e556ffbe441
DarlsyLida <57491703+DarlsyLida1@users.noreply.github.com>
2025-07-30 15:41:00 +0530

    Lab 2 Package Exercise

---
+++ person/PersonalDetails.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package person;
+
+
+
+public class PersonalDetails {
+    public void displayDetails() {
+        System.out.println("Name      : Darlsy Lida");
+        System.out.println("Address   : 123 Main Street, City, Country");
+        System.out.println("Email     : darlsy@example.com");
+        System.out.println("Mobile No : +91-9876543210");
+    }
+}
---
+++ person/person.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import person.PersonalDetails;
+
+/**
+ *
+ * @author fymsc49
+ */
+public class person {
+    public static void main(String[] args) {
+        PersonalDetails p = new PersonalDetails();
+        p.displayDetails();
+    }
+    
+}
